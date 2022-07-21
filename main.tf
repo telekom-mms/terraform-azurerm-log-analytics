8eec71534540329289facf954736f1b1cbf1d857
mima <Michaela.Mattes@t-systems.com>
2022-07-21 13:55:18 +0200

    add more attributes

--- main.tf
+++ main.tf
@@ -18,3 +18,21 @@ resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
   reservation_capacity_in_gb_per_day = local.log_analytics_workspace[each.key].reservation_capacity_in_gb_per_day
   tags                               = local.log_analytics_workspace[each.key].tags
 }
+
+resource "azurerm_log_analytics_solution" "log_analytics_solution" {
+  for_each = var.log_analytics_solution
+
+  solution_name          = local.log_analytics_solution[each.key].solution_name == "" ? each.key : local.log_analytics_solution[each.key].solution_name
+  location               = local.log_analytics_solution[each.key].location
+  resource_group_name    = local.log_analytics_solution[each.key].resource_group_name
+  workspace_resource_id  = local.log_analytics_solution[each.key].workspace_resource_id
+  workspace_name         = local.log_analytics_solution[each.key].workspace_name
+
+  plan {
+    publisher= local.log_analytics_solution[each.key].plan.publisher
+    product= local.log_analytics_solution[each.key].plan.product
+    promotion_code = local.log_analytics_solution[each.key].plan.promotion_code
+  }
+
+  tags = local.log_analytics_solution[each.key].tags
+}
--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "log_analytics_workspace" {
   default     = {}
   description = "resource definition, default settings are defined within locals and merged with var settings"
 }
+variable "log_analytics_solution" {
+  type        = any
+  default     = {}
+  description = "resource definition, default settings are defined within locals and merged with var settings"
+}
 
 locals {
   default = {
@@ -17,12 +22,34 @@ locals {
       reservation_capacity_in_gb_per_day = null
       tags                               = {}
     }
+    log_analytics_solution = {
+      solution_name = ""
+      plan = {
+        publisher = "Microsoft"
+        promotion_code = null
+      }
+    }
   }
 
   # compare and merge custom and default values
+  log_analytics_solution_values = {
+    for log_analytics_solution in keys(var.log_analytics_solution) :
+    log_analytics_solution => merge(local.default.log_analytics_solution, var.log_analytics_solution[log_analytics_solution])
+  }
+
   # merge all custom and default values
   log_analytics_workspace = {
     for log_analytics_workspace in keys(var.log_analytics_workspace) :
     log_analytics_workspace => merge(local.default.log_analytics_workspace, var.log_analytics_workspace[log_analytics_workspace])
+  }
+  log_analytics_solution = {
+    for log_analytics_solution in keys(var.log_analytics_solution) :
+    log_analytics_solution => merge(
+      local.log_analytics_solution_values[log_analytics_solution],
+      {
+        for config in ["plan"] :
+        config => merge(local.default.log_analytics_solution[config], local.log_analytics_solution_values[log_analytics_solution][config])
+      }
+    )
   }
 }
